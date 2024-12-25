62d1e926f81b68a9b6d1d850f1d8345e1e54a093
Gurpinder009 <gurpinders1511@gmail.com>
2024-12-25 12:46:50 -0800

    software tag added

--- src/components/pages/HomePageComponent.jsx
+++ src/components/pages/HomePageComponent.jsx
@@ -28,7 +28,7 @@ export default function HomePageComponent() {
                 <StyledHomePageItem variants={FadeLeftAnimationVariant} initial="hidden" whileInView={"visible"}>
                     <p>Hello, my name is </p>
                     <h1>Gurpinder <br /> Singh Grewal</h1>
-                    <p>I am a Web Developer, currently working as freelancer for local community. For more information regarding me and my work look at my <b>LinkedIn</b> and <b>Github</b> profiles. you can even stay in touch with me using popular social media platforms, links given below.</p>
+                    <p>I am a Software Developer, currently working as freelancer for local community. For more information regarding me and my work look at my <b>LinkedIn</b> and <b>Github</b> profiles. you can even stay in touch with me using popular social media platforms, links given below.</p>
                     <StyledCVButton  variants={ButtonHoverEffect} whileHover={"hover"} href="CV.pdf" download={true} >Download CV</StyledCVButton>
                 </StyledHomePageItem>
                 <StyledHomePageItem $data_image_url={"/arrow_left2.jpg"}   variants={FadeRightInvertAnimationVariant} initial="hidden" whileInView={"visible"}/>
