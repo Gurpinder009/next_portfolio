c4e38bf2230a1916cf6fe3985f0ecf14d0456a8c
gurpinder009 <gurpinders1511@gmail.com>
2025-01-05 09:35:05 -0800

    Resume file updated

--- src/components/pages/HomePageComponent.jsx
+++ src/components/pages/HomePageComponent.jsx
@@ -29,7 +29,7 @@ export default function HomePageComponent() {
                     <p>Hello, my name is </p>
                     <h1>Gurpinder <br /> Singh Grewal</h1>
                     <p>I am a Software Developer, currently working as freelancer for local community. For more information regarding me and my work look at my <b>LinkedIn</b> and <b>Github</b> profiles. you can even stay in touch with me using popular social media platforms, links given below.</p>
-                    <StyledCVButton  variants={ButtonHoverEffect} whileHover={"hover"} href="CV.pdf" download={true} >Download CV</StyledCVButton>
+                    <StyledCVButton  variants={ButtonHoverEffect} whileHover={"hover"} href="GurpinderSinghResume.pdf" download={true} >Download My Resume</StyledCVButton>
                 </StyledHomePageItem>
                 <StyledHomePageItem $data_image_url={"/arrow_left2.jpg"}   variants={FadeRightInvertAnimationVariant} initial="hidden" whileInView={"visible"}/>
         </StyledHomePageContainer>
